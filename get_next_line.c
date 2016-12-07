1d296790002c3bd2b9b150d0d8a3e8582eb1a50c
Natansab <natansab@gmail.com>
2016-12-07 16:39:36 +0100

    basic test ok, moulitest 1 error, 0 leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/24 17:35:41 by nsabbah           #+#    #+#             */
-/*   Updated: 2016/12/06 19:44:41 by nsabbah          ###   ########.fr       */
+/*   Updated: 2016/12/07 16:38:50 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,57 +18,65 @@
 #include <stdio.h>
 #include <string.h>
 
-int ft_tmp2line(char **tmp, char **line)
+int ft_stock2line(char **stock, char **line)
 {
   int len;
 
-  /* If there is a '\n in tmp, put it in line */
-  if (strchr(*tmp, '\n') != NULL)
+  /* If there is a '\n in stock, put it in line */
+  if (ft_strchr(*stock, '\n') != NULL)
   {
-    len = strchr(*tmp, '\n') - *tmp;
-    line[0] = ft_strsub(*tmp, 0, len);
-    *tmp = strchr(*tmp, '\n') + 1;
+    len = ft_strchr(*stock, '\n') - *stock;
+    line[0] = ft_strsub(*stock, 0, len);
+    *stock = ft_strchr(*stock, '\n') + 1;
     return (1);
   }
-  /* If there is no newline in tmp, copy tmp in line [0] */
-  line[0] = strdup(*tmp);
-  *tmp = NULL;
+  /* If there is no newline in stock, copy stock in line [0] */
+  line[0] = ft_strdup(*stock);
+  *stock = NULL;
   return (0);
 }
 
 int get_next_line(const int fd, char **line)
 {
   int ret;
-  static char *tmp = NULL;
+  static char *stock = NULL;
   int len;
   char *str;
+  char *tmp;
 
   if (line == NULL)
     return (-1);
-  /* Why do I need to malloc line[0] ?*/
-  line[0] = (char *)malloc(1);
+  line[0] = ft_strnew(BUFF_SIZE);
+  if (stock != NULL)
+ //   {
+      if (ft_stock2line(&stock, &line[0]))
+        return (1);
+  //    stock = NULL;
+   // }
 
-  if (tmp != NULL)
-    if (ft_tmp2line(&tmp, &line[0]))
-      return (1);
-
-  str = (char *)malloc(BUFF_SIZE);
+  str = ft_strnew(BUFF_SIZE);
   while ((ret = read(fd, str, BUFF_SIZE)))
   {
     if (ret == -1)
       return (-1);
     str[ret] = '\0';
-    line[0] = ft_strjoin(line[0], str);
-    if (strchr(line[0], '\n') != NULL)
+    tmp = ft_strdup(line[0]);
+    free (line[0]);
+    line[0] = ft_strjoin(tmp, str);
+    free(tmp);
+    if (ft_strchr(line[0], '\n') != NULL)
     {
-      len = strchr(line[0], '\n') - line[0];
-      tmp = strdup(&(line[0][len + 1]));
-      line[0][len] = '\0';
+      len = ft_strchr(line[0], '\n') - line[0];
+      stock = ft_strdup(&(line[0][len + 1]));
+      tmp = ft_strdup(line[0]);
+      free (line[0]);
+      line[0] = ft_strsub(tmp, 0, len);
+      free(tmp);
       break;
     }
   }
   free(str);
-  if (ret == 0 && strlen(line[0]) == 0)
+  if (ret == 0 && ft_strlen(line[0]) == 0)
     return (0);
   return (1);
 }
